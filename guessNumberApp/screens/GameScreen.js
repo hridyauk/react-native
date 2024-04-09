0b1a9552ba91d0664a27750cedf32eec3c453ec4
hridyauk <h180@mylangara.ca>
2024-04-09 14:21:28 -0700

    rearranging components based on screen orientation change

--- guessNumberApp/screens/GameScreen.js
+++ guessNumberApp/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Alert, FlatList } from "react-native";
+import { View, StyleSheet, Alert, FlatList, useWindowDimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
@@ -23,12 +23,13 @@ let maxBoundary = 100;
 
 function GameScreen({ userNumber, onGameOver }) {
   const initialGuess = generateRandomBetween(
-    minBoundary,
-    maxBoundary,
+    1,
+    100,
     userNumber
   );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [guessRounds, setGuessRounds] = useState([initialGuess]);
+  const { width, height } = useWindowDimensions()
 
   useEffect(() => {
     if (currentGuess === userNumber) {
@@ -70,27 +71,52 @@ function GameScreen({ userNumber, onGameOver }) {
 
   const guessRoundLength = guessRounds.length;
 
+  let content = <>
+    <NumberContainer>{currentGuess}</NumberContainer>
+    <Card>
+      <InstructionText style={styles.instructionText}>
+        Higher or Lower?
+      </InstructionText>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            <Ionicons name="remove-sharp" size={24} color="white" />
+          </PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
+            <Ionicons name="add-sharp" size={24} color="white" />
+          </PrimaryButton>
+        </View>
+      </View>
+    </Card>
+  </>
+
+  if(width > 500){
+    content = <>
+      {/* <InstructionText style={styles.instructionText}>
+        Higher or Lower?
+      </InstructionText> */}
+      <View style={styles.buttonContainerWide}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            <Ionicons name="remove-sharp" size={24} color="white" />
+          </PrimaryButton>
+        </View>
+        <NumberContainer>{currentGuess}</NumberContainer>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
+            <Ionicons name="add-sharp" size={24} color="white" />
+          </PrimaryButton>
+        </View>
+      </View>
+    </>
+  }
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      <NumberContainer>{currentGuess}</NumberContainer>
-      <Card>
-        <InstructionText style={styles.instructionText}>
-          Higher or Lower?
-        </InstructionText>
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              <Ionicons name="remove-sharp" size={24} color="white" />
-            </PrimaryButton>
-          </View>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
-              <Ionicons name="add-sharp" size={24} color="white" />
-            </PrimaryButton>
-          </View>
-        </View>
-      </Card>
+      {content}
       <View style={styles.listContainer}>
         {/* {guessRounds.map(guessRounds => <Text key={guessRounds}>{guessRounds}</Text>)} */}
         <FlatList
@@ -113,7 +139,8 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 24
+    padding: 24,
+    alignItems: 'center'
   },
   buttonsContainer: {
     flexDirection: "row"
@@ -127,5 +154,9 @@ const styles = StyleSheet.create({
   listContainer: {
       flex: 1,
       padding: 16
+  },
+  buttonContainerWide: {
+    flexDirection: 'row',
+    alignItems: "center",
   }
 });
