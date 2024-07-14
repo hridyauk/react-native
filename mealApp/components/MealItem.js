5ad0f34549676d4ebc37de6f4d099dce196aa34c
Hridya Unnikrishnan <133180993+hridyauk@users.noreply.github.com>
2024-07-13 20:29:41 -0700

    meal app almost complete

---
+++ mealApp/components/MealItem.js
@@ -0,0 +1,86 @@
+import { useNavigation } from "@react-navigation/native";
+import {
+  Image,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import MealDetails from "./MealDetails";
+
+function MealItem({
+  id,
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+}) {
+  const navigation = useNavigation();
+
+  function selectMealHandler() {
+    navigation.navigate("MealDetail", {
+      mealId: id,
+    });
+  }
+
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+        onPress={selectMealHandler}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.imageStyle} />
+            <Text style={styles.titleStyle}>{title}</Text>
+          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+            textStyle={styles.textStyle}
+          />
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 16,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  imageStyle: {
+    width: "100%",
+    height: 200,
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  textStyle: {
+    color: "white",
+  },
+});
